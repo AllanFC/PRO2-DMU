abac8bb1fa54cb946eb03ce67b4622e15e11d778
AllanFC <allan.christiansen@live.dk>
2022-08-28 13:12:50 +0200

    Ex04b

--- L01-Collections/src/Ex04/Ex04.java
+++ L01-Collections/src/Ex04/Ex04.java
@@ -2,14 +2,14 @@ package Ex04;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Ex04 {
     public static void main(String[] args) {
         String path = "L01-Collections/src/Ex04/Gjoengehoevdingen.txt";
-        useTreeSet(path); // 8681 unikkke ord ud af 93428 total antal ord
+        // 8681 unikkke ord ud af 93428 total antal ord
+        //useTreeSet(path);
+        useTreeMap(path);
     }
 
     public static void useTreeSet(String path){
@@ -41,7 +41,37 @@ public class Ex04 {
         } catch(FileNotFoundException ex){
             System.out.println("Hej jeg er en fejl");
         }
-        System.out.printf("%d unikkke ord ud af %d total antal ord\n", uniqueCounter, wordCounter);
-        System.out.println(treeSet);
+
+    }
+
+    public static void useTreeMap(String path){
+        File in = new File(path);
+        String w;
+        Map<String, Integer> treeMap = new TreeMap<>();
+        int uniqueCounter = 0;
+        int wordCounter = 0;
+        try(Scanner scanner = new Scanner(in)){
+            while(scanner.hasNext()){
+                w = scanner.next().toLowerCase().replaceAll("[^a-å’]","");
+                if(!w.equals("")){
+                    if(treeMap.containsKey(w)){
+                        treeMap.replace(w,treeMap.get(w)+1);
+                        wordCounter++;
+                    }else{
+                        treeMap.put(w,1);
+                        uniqueCounter++;
+                    }
+                }
+            }
+        } catch(FileNotFoundException ex){
+            System.out.println("Hej jeg er en fejl");
+        }
+
+        for(String key : treeMap.keySet()){
+            if(treeMap.get(key) >= 1000){
+                System.out.println(key+": " + treeMap.get(key));
+            }
+        }
+        System.out.printf("%d unikke ord ud af %d total antal ord\n", uniqueCounter, wordCounter);
     }
 }
