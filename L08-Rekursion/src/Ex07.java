f7b1b5dcf44ada10e3f5609aecd1c5dbcf443a63
AllanFC <allan.christiansen@live.dk>
2022-09-19 08:56:37 +0200

    brug af "SEJ" if statement

--- L08-Rekursion/src/Ex07.java
+++ L08-Rekursion/src/Ex07.java
@@ -4,9 +4,12 @@ public class Ex07 {
     }
 
     public static int tiles(int n){
-        if(n >= 3){
-            return tiles(n-1)+tiles(n-2);
-        }
-        return n;
+        return n >= 3 ? tiles(n-1)+tiles(n-2) : n;
     }
+//    public static int tiles(int n){
+//        if(n >= 3){
+//            return tiles(n-1)+tiles(n-2);
+//        }
+//        return n;
+//    }
 }
