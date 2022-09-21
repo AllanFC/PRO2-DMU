d8435e2009ee8f09908973da4488f05f51d74f5a
AllanFC <allan.christiansen@live.dk>
2022-09-21 09:05:48 +0200

    towers of hanoi

--- L09-Rekursion2/src/Towers.java
+++ L09-Rekursion2/src/Towers.java
@@ -4,20 +4,20 @@ public class Towers {
     public static void main(String args[])
     {
         int n = 5;
-        towerOfHanoi(n,1,3);
+        System.out.printf("Antal flytninger: %d\n", towerOfHanoi(n,1,3));
     }
 
     static int towerOfHanoi(int n, int fra, int til)
     {
         if (n == 1)
         {
-            System.out.println("Flyt fra " + fra + " til " + til);
+            //System.out.println("Flyt fra " + fra + " til " + til);
             count++;
             return 0;
         }
         int helper = 6 - fra - til;
         towerOfHanoi(n-1, fra, helper);
-        System.out.println("Flyt fra " + fra + " til " + til);
+        //System.out.println("Flyt fra " + fra + " til " + til);
         count++;
         towerOfHanoi(n-1, helper, til);
         return count;
