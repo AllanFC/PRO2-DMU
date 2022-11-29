89744c09faec2864b283ac11dc42c3a4927385cd
AllanFC <allan.christiansen@live.dk>
2022-11-29 23:10:09 +0100

    Ex02-04. Ex04 got a bug with removeNext.

---
+++ L24-Queue_Deque/src/Ex01c/main.java
@@ -0,0 +1,26 @@
+package Ex01c;
+
+
+
+public class main {
+    public static void main(String[] args) {
+        QueueI<String> q = new ArrayQueue<>();
+        q.add("Tom");;
+        q.add("Diana");
+        q.add("Harry");
+        q.add("Thomas");
+        q.add("Bob");
+        q.add("Brian");
+        System.out.println(q);
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println();
+        System.out.println(q);
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println();
+        System.out.println(q);
+    }
+}
