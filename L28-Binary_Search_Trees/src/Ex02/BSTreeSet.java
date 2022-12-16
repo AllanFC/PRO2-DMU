96098ac2bfe63e8fa6437dceb1cf6daa8c8568c7
AllanFC <allan.christiansen@live.dk>
2022-12-16 09:55:24 +0100

    Ex02 binary search trees

---
+++ L28-Binary_Search_Trees/src/Ex02/BSTreeSet.java
@@ -0,0 +1,233 @@
+package Ex02;
+
+/**
+ * BSTreeSet implements a sorted set, sorted according to the elements' natural ordering.
+ * Elements in the set of type E must implement the Comparable<E> interface.
+ * Note: Equal elements are not allowed in the set. Equality of two elements is
+ * decided by the elements' compareTo() method.
+ * Note: Elements must have a compareTo() method that is consistent with equals(),
+ * that is, e1.compareTo(e2) == 0 <=> e1.equals(e2) == true.
+ */
+public class BSTreeSet<E extends Comparable<E>> {
+    // BSTreeSet is implemented as a binary search tree.
+
+    private Node root;
+
+    /**
+     * Create an empty BSTreeSet.
+     */
+    public BSTreeSet() {
+        root = null;
+    }
+
+    //-------------------------------------------------------------------------
+
+    // Return a NodePair consisting of the node
+    // containing 'element' and the node's parent.
+    // If root contains 'element', node is root, and parent is null.
+    // If no node contains 'element', node is null,
+    // and parent is the node where 'element' would be added.
+    // Pre: The tree is not empty.
+    private NodePair findNodeAndParent(E element) {
+        Node parent = null;
+        Node node = root;
+        boolean found = false;
+        while (!found && node != null) {
+            int d = element.compareTo(node.data);
+            if (d == 0) {
+                found = true;
+            } else {
+                parent = node;
+                if (d < 0) {
+                    node = node.left;
+                } else {
+                    node = node.right;
+                }
+            }
+        }
+        return new NodePair(node, parent);
+    }
+
+    private class NodePair {
+        private final Node node;
+        private final Node parent;
+
+        public NodePair(Node node, Node parent) {
+            this.node = node;
+            this.parent = parent;
+        }
+    }
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * Add 'element' to the set, if 'element' is not already present.
+     * Return true, if 'element' is added.
+     */
+    public boolean add(E element) {
+        if(root == null){
+            root = new Node(element);
+            return true;
+        }
+
+        NodePair pair = findNodeAndParent(element);
+        if(pair.node != null){
+            return false;
+        }
+
+        Node newNode = new Node(element);
+        Node parent = pair.parent;
+        if(newNode.data.compareTo(parent.data) > 0){
+            parent.right = newNode;
+        } else {
+            parent.left = newNode;
+        }
+
+        return true;
+    }
+
+    /**
+     * Return true, if the set contains 'element'.
+     */
+    public boolean contains(E element) {
+        if(root == null){
+            return false;
+        }
+        return findNodeAndParent(element).node != null;
+    }
+
+    /**
+     * Remove 'element' from the set, if it is present.
+     * Return true, if 'element' is removed.
+     */
+    public boolean remove(E element) {
+        // guard: tree is empty
+        if (root == null)
+            return false;
+
+        // look for node containing element
+        NodePair pair = this.findNodeAndParent(element);
+        Node toBeRemoved = pair.node;
+        Node parent = pair.parent;
+
+        // guard: element is not in the tree
+        if (toBeRemoved == null)
+            return false;
+
+        if (toBeRemoved.left == null || toBeRemoved.right == null) {
+            // one child is null (or both)
+
+            // child is the not-null child of toBeRemoved (or null if both children are  null)
+            Node child = (toBeRemoved.left != null) ? toBeRemoved.left : toBeRemoved.right;
+
+            if (parent == null) {
+                // toBeRemoved is the root
+                root = child;
+            } else if (parent.left == toBeRemoved) {
+                parent.left = child;
+            } else {
+                parent.right = child;
+            }
+        } else {
+            // both children of toBeRemoved are not null
+
+            // find the smallest element of the right subtree
+            Node parentOfSmallest = toBeRemoved;
+            Node smallest = toBeRemoved.right;
+            while (smallest.left != null) {
+                parentOfSmallest = smallest;
+                smallest = smallest.left;
+            }
+            // smallest now contains the smallest child in right subtree (and its left child is null)
+
+            // move content of smallest to toBeRemoved, remove smallest
+            toBeRemoved.data = smallest.data;
+            if (parentOfSmallest == toBeRemoved) {
+                parentOfSmallest.right = smallest.right;
+            } else {
+                parentOfSmallest.left = smallest.right;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Returns the largest entry in the binary search tree.
+     * If the tree is empty, null is returned.
+     **/
+    public E max(){
+        if(root == null){
+            return null;
+        }
+
+        Node node = root;
+        while(node.right != null){
+            node = node.right;
+        }
+        return node.data;
+    }
+    /**
+     * Removes and returns the smallest entry in the binary search tree.
+     * If the tree is empty, null is returned.
+     **/
+    public E removeMin() {
+        if (root == null) {
+            return null;
+        }
+
+        Node parent = null;
+        Node node = root;
+        while (node.left != null) {
+            parent = node;
+            node = node.left;
+        }
+        Node toBeRemoved = node;
+        Node child = toBeRemoved.right;
+        if (parent == null) {
+            // toBeRemoved is the root
+            root = child;
+        } else if (parent.left == toBeRemoved) {
+            parent.left = child;
+        }
+        return toBeRemoved.data;
+    }
+
+    /**
+     * Return a string containing all elements in sorted order.
+     */
+    @Override
+    public String toString() {
+        if(root == null){
+            return "[]";
+        }
+        StringBuilder sb = new StringBuilder("[");
+        printInorderHelper(root, sb);
+        sb.delete(sb.lastIndexOf(","), sb.lastIndexOf(",")+2);
+        sb.append("]");
+        return sb.toString();
+    }
+
+    private void printInorderHelper(Node node, StringBuilder sb){
+        if(node.left != null) {
+            printInorderHelper(node.left, sb);
+        }
+        sb.append(node.data).append(", ");
+        if(node.right != null){
+            printInorderHelper(node.right, sb);
+        }
+    }
+
+    //-------------------------------------------------------------------------
+
+    private class Node {
+        private E data;
+        private Node left;
+        private Node right;
+
+        public Node(E data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+}
